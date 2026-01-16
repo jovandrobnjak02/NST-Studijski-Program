1788b3b5e051f592ae40cb80061277fffe7c6d42
jovandrobnjak02 <drobnjakj02@gmail.com>
2026-01-16 22:38:03 +0100

    Project structure

---
+++ src/main/java/com/example/studijskiprogram/StudijskiProgramApplication.java
@@ -0,0 +1,13 @@
+package com.example.studijskiprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudijskiProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudijskiProgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/studijskiprogram/StudijskiProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studijskiprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudijskiProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
